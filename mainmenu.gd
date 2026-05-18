53f669294eb8b7cc0fb9549fc9c88735d531500c
Harper Nicholson <harper@harpernicholson.ca>
2026-05-18 11:28:19 -0400

    update

--- mainmenu.gd
+++ mainmenu.gd
@@ -19,6 +19,19 @@ func set_gui_node_inputs_disabled(_disabled := true):
 		node.disabled = _disabled
 
 func move_menu(spot : MENU_SPOT = MENU_SPOT.CENTER):
+	
+	var target_position_x : float
+	
+	match spot:
+		MENU_SPOT.LEFT: target_position_x = -360.0
+		MENU_SPOT.CENTER: target_position_x = 0.0
+		MENU_SPOT.RIGHT: target_position_x = 360.0
+	
+	var menu_tween : Tween = get_tree().create_tween()
+	
+	menu_tween.tween_property($Control, "position:x", target_position_x, 1.0)
 	#tween the menu position to a spot, revealing the class selector etc for the appropriate team.
+	
+	await menu_tween.finished
+	set_gui_node_inputs_disabled(false)
 	#on finished, re-enable gui nodes
-	pass
